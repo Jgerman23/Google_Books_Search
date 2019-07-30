afad0536c0c3e007f5d978894b3511e3bff3ff63
Justin German <justin.german23@gmail.com>
2019-07-30 15:48:29 -0500

    finshed components for app

---
+++ books/client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Jumbotron = ({children}) => (
+    <div className="jumbotron jumbotron-fluid">
+        <div style={{height: 150, clear:"both", paddingTop:10, paddingBottom:20, textAlign: "center"}} className="container">
+            {children}
+        </div>
+    </div>
+);
+
+export default Jumbotron;
